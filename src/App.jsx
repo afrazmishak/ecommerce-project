59093f59558ad910f8317e44a509168b8ab78dbc
Afraz Mishak <afrazmishak@gmail.com>
2025-11-27 14:47:36 +0400

    Convert home and checkout to async await

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,21 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-        setCart(response.data);
-      })
+    const fetchAppDate = async () => {
+      const response = await axios.get('/api/cart-items?expand=product')
+      setCart(response.data);
+    }
+
+    fetchAppDate()
   }, []);
 
 
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart}/>} />
-      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
-      <Route path="orders" element={<OrdersPage cart={cart}/>} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
 
   )
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,23 +4,22 @@ import { Header } from '../../components/Header'
 import './HomePage.css'
 import { ProductsGrid } from './ProductsGrid'
 
-export function HomePage( {cart} ) {
+export function HomePage({ cart }) {
     const [products, setProducts] = useState([]);
-    
+
 
     useEffect(() => {
-        axios.get('/api/products')
-        .then((response) => {
+        const getHomeDate = async () => {
+            const response = await axios.get('/api/products')
             setProducts(response.data)
-        });
-
-        
+        }
+        getHomeDate()
     }, []);
 
     return (
         <>
             <title>Buy You Shopping</title>
-            <Header cart={cart}/>
+            <Header cart={cart} />
 
             <div className="home-page">
                 <ProductsGrid products={products} />
