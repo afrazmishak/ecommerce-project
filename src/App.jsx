dc95a490b933ded80f96779c356ba6b56309e497
Afraz Mishak <afrazmishak@gmail.com>
2025-11-29 14:09:12 +0400

    Create add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,21 @@ import axios from 'axios';
 function App() {
   const [cart, setCart] = useState([]);
 
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product')
+    setCart(response.data);
+  };
+
   useEffect(() => {
-    const fetchAppDate = async () => {
-      const response = await axios.get('/api/cart-items?expand=product')
-      setCart(response.data);
+    const fetchData = async () => {
+      await loadCart();
     }
-
-    fetchAppDate()
+    fetchData();
   }, []);
-
-
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
